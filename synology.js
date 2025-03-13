168358fb2f62ffc6378a7586731da1a043b004d2
Stepan Omyshev <omyshev.s@inbox.ru>
2025-03-13 16:31:11 +0500

    Initial commit

--- synology.js
+++ synology.js
@@ -1,10 +1,13 @@
 ;(function () {
 	'use strict'
 
-	// Создаём новый компонент
 	function CustomPage(object) {
 		var html = $('<div class="custom-page">')
+
 		html.append('<h1 style="text-align:center;">Моя кастомная страница</h1>')
+		html.append(
+			'<div style="text-align:center;"><button id="closePage">Закрыть</button></div>'
+		)
 
 		this.create = function () {
 			return html
@@ -15,28 +18,26 @@
 		this.destroy = function () {
 			html.remove()
 		}
+
+		html.find('#closePage').on('click', function () {
+			Lampa.Activity.backward() // Закрытие страницы
+		})
 	}
 
-	// Добавляем компонент в Lampa
-	Lampa.Component.add('custom_page', CustomPage)
+	Lampa.Component.add('custom_page', CustomPage) // Добавляем компонент в Lampa
 
-	// Функция для открытия страницы
 	function openCustomPage() {
 		Lampa.Activity.push({
 			url: '',
 			title: 'Кастомная страница',
-			component: 'custom_page', // Указываем зарегистрированный компонент
+			component: 'custom_page',
 			page: true,
-			onBack: function () {
-				Lampa.Activity.backward()
-			},
 		})
 	}
 
-	// Добавляем кнопку в меню
 	function addMenuButton() {
 		var menu = $('.menu .menu__list').eq(0)
-		var button = $('<li class="menu__item selector">')
+		var button = $('<li class="menu__item selector focus">')
 			.append('<div class="menu__text">Моя страница</div>')
 			.on('hover:enter', function () {
 				openCustomPage()
@@ -45,7 +46,6 @@
 		menu.append(button)
 	}
 
-	// Запускаем после загрузки Lampa
 	Lampa.Listener.follow('app', function (e) {
 		if (e.type === 'ready') {
 			addMenuButton()
