5e68c8658c98a5df830660c1b49d6787a03988aa
Stepan Omyshev <omyshev.s@inbox.ru>
2025-03-13 16:26:44 +0500

    Initial commit

--- synology.js
+++ synology.js
@@ -2,41 +2,33 @@
 	'use strict'
 
 	// Создаём новый компонент
-	function createCustomPage(object) {
+	function CustomPage(object) {
 		var html = $('<div class="custom-page">')
 		html.append('<h1 style="text-align:center;">Моя кастомная страница</h1>')
 
-		var last_scroll = 0
-
-		// Запоминаем позицию скролла
-		this.start = function () {
-			$('body').addClass('no-scroll')
-			html.on('scroll', function () {
-				last_scroll = html.scrollTop()
-			})
+		this.create = function () {
+			return html
 		}
 
-		// Удаляем при выходе
+		this.start = function () {
+			$('body').addClass('no-scroll')
+		}
+
 		this.destroy = function () {
 			$('body').removeClass('no-scroll')
 			html.remove()
 		}
-
-		// Возвращаем рендер страницы
-		this.render = function () {
-			return html
-		}
 	}
 
 	// Добавляем компонент в систему Lampa
-	Lampa.Component.add('custom_page', createCustomPage)
+	Lampa.Component.add('custom_page', CustomPage)
 
 	// Функция для открытия страницы
 	function openCustomPage() {
 		Lampa.Activity.push({
 			url: '',
 			title: 'Кастомная страница',
-			component: 'custom_page', // Правильное подключение
+			component: 'custom_page', // Используем имя зарегистрированного компонента
 			page: true,
 		})
 	}
@@ -46,7 +38,7 @@
 		var menu = $('.menu .menu__list').eq(0)
 		var button = $('<li class="menu__item selector">')
 			.append('<div class="menu__text">Моя страница</div>')
-			.on('hover:enter', openCustomPage) // Вызываем открытие страницы
+			.on('hover:enter', openCustomPage)
 
 		menu.append(button)
 	}
